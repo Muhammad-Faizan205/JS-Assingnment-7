129201c989ca3dbc330fcea1175dff23e653ffcd
code-with-smart <smartfaiza53@gmail.com>
2023-10-09 16:27:34 +0500

    js practice

---
+++ app.js
@@ -0,0 +1,108 @@
+// ---------------------- CHAPTER 46,48(Events) -------------------- //
+
+// Ques ------ 2
+
+// function hide()
+// {
+//     document.getElementById("elementToHide");
+//     elementToHide.style.display = 'none';
+// }
+
+
+// Ques ------ 11
+
+// function remove()
+// {
+//     document.getElementById("link");
+//     link.style.display = "none";
+// }
+
+
+// -------------------------- CHAPTER 49,50(Reading and Setting field values) ------------------ //
+
+// Ques ------ 1
+
+// function readvalue()
+// {
+//    var input =  document.getElementById("userinput")
+//    console.log(input.value)
+// }
+
+
+// Ques ------ 2
+
+// function userinput()
+// {
+// var check = document.getElementById("num1")
+// if(check.checked)
+// {
+//     console.log("Check box check")
+// }
+// else{
+//     console.log("Check box is not check")
+// }
+// }
+
+
+// Ques ------ 3
+
+//  function click() {
+// var select = document.getElementById("mySelect");
+// var button = document.getElementById("myButton");
+//   var selectedValue = select.value;  
+//   console.log("Selected Value: " + selectedValue.value);
+// };
+
+
+// Ques ------ 5
+
+// function updateStatus() {
+//     var maritalStatusSelect = document.getElementById("maritalStatus");
+//     var statusField = document.getElementById("status");
+//     if (maritalStatusSelect.value === "Unmarried") {
+
+//       statusField.value = "available";
+//     } else {      
+//       statusField.value = "";
+//     }
+//   }
+//   updateStatus();
+
+
+// Ques ------ 6
+
+// function check()
+// {
+//     var inputfield = document.getElementById("input");
+//     var myButton = document.getElementById("button");
+//     if(inputfield.value === ""){
+//         inputfield.value ="usman"
+//     }
+// }
+
+
+// Ques ------ 7
+
+// function Form()
+// {
+//     var myname = document.getElementById("Myname");
+//     var fathername = document.getElementById("Fathername");
+//     var phone = document.getElementById("Contact");
+
+//    if(myname.value === "")
+//    {
+//     alert("Enter Your Name");
+//    }
+//    else if(fathername.value === "")
+//    {
+//     alert("Enter your Father name");
+//    }
+//    else if(phone.value === "")
+//    {
+//     alert("Enter Phone Number");
+//    }
+//    else{
+//     alert("Your Form Fill")
+//    }
+
+// }
